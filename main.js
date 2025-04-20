074fa4976655ae7920456e28b8aa24d3c73baa85
Thiago Andrade <thiagopollinho@gmail.com>
2025-04-20 00:00:26 -0300

    updating files

---
+++ main.js
@@ -0,0 +1,99 @@
+const buscarBtn = document.getElementById("buscar-clima")
+const btnLoc = document.getElementById('btn-loc')
+const resultTemp = document.getElementById("temperatura")
+const resultClima = document.getElementById("clima")
+const resultUmidade = document.getElementById("umidade")
+const resultPressao = document.getElementById("pressao")
+
+buscarBtn.addEventListener("click", () => {
+    const buscarInput = document.getElementById("cidade")
+    const input = buscarInput.value
+    const result = document.getElementById("resultado")
+    const resultLoc = document.getElementById("resultado-loc")
+
+    resultLoc.innerHTML = ""
+
+
+    const apiKey = "3f77da438c85bb37b4cb2f0517753764"
+    const urlTempo = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric&lang=pt_br`
+
+    fetch(urlTempo)
+        .then(response => {
+            if (!response.ok) {
+                return response.json()
+                    .then(errorData => {
+                        throw new Error(errorData.message)
+                    })
+            }
+            return response.json()
+        })
+        .then(dados => {
+            const temp = dados.main.temp
+            const umidade = dados.main.humidity
+            const descricao = dados.weather[0].description;
+            const pressao = dados.main.pressure
+            result.innerHTML = `Tempo agora em: ${input}`
+            resultTemp.innerHTML = `Temperatura: ${temp}°C`
+            resultClima.innerHTML = `Clima: ${descricao}`
+            resultUmidade.innerHTML = `Umidade: ${umidade}%`
+            resultPressao.innerHTML = `Pressão: ${pressao}hPa`
+
+            buscarInput.value = ""
+            console.log(dados)
+        })
+        .catch(error => {
+            result.innerHTML = `Erro: ${error.message}`//esse parametro estava vazio, ja que eu so estava passando throw new error la em cima
+        })
+
+})
+
+btnLoc.addEventListener("click", () => {
+    const resultLoc = document.getElementById("resultado-loc")
+    const result = document.getElementById("resultado")
+    result.innerHTML = ""
+
+    const urlLoc = "http://ip-api.com/json/"
+    fetch(urlLoc)
+    .then(response => {
+        console.log(response)
+        return response.json()
+    })
+    .then(data => {
+        console.log(data)
+        const city = data.city
+        const region = data.region
+        resultLoc.innerHTML = `Tempo agora em ${city}, ${region}`
+        
+        
+            const apiKey = "3f77da438c85bb37b4cb2f0517753764"
+            const urlTempo = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`
+
+            fetch(urlTempo)
+                .then(response => {
+                    if (!response.ok) {
+                        return response.json()
+                            .then(errorData => {
+                                throw new Error(errorData.message)
+                            })
+                    }
+                    return response.json()
+                })
+                .then(dados => {
+                    const temp = dados.main.temp
+                    const umidade = dados.main.humidity
+                    const descricao = dados.weather[0].description;
+                    const pressao = dados.main.pressure
+
+                    resultTemp.innerHTML = `Temperatura: ${temp}°C`
+                    resultClima.innerHTML = `Clima: ${descricao}`
+                    resultUmidade.innerHTML = `Umidade: ${umidade}%`
+                    resultPressao.innerHTML = `Pressão: ${pressao}hPa`
+                    console.log(dados)
+                })
+                .catch(error => {
+                    result.innerHTML = `Erro: ${error.message}`
+                })
+        })
+})
+
+
